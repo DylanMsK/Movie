024dfa602290e88a241818a9f753c471bc76979a
DylanMsK <kms920612@gmail.com>
2019-05-09 22:06:56 +0900

    GET movie/ url에 영화 리스트 조회기능 구현

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Country, Genre, Score, Movie
 
 # Register your models here.
+admin.site.register(Country)
+admin.site.register(Genre)
+admin.site.register(Score)
+admin.site.register(Movie)
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .models import Movie
+from .serializer import MovieSerializer
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 # Create your views here.
+@api_view(['GET'])
+def movie_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
